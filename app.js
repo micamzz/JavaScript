6e102e479a71d3f1395905928e950fa4be1be410
Micaela Mazza <micamazza@hotmail.com>
2021-07-26 13:02:48 -0300

    probando js

---
+++ app.js
@@ -0,0 +1,10 @@
+let name = prompt(' Ingrese su nombre');
+let age = Number(prompt('Ingrese su edad'))
+
+// condicional
+
+if (age >= 18) {
+    alert('Podes ingresar al bar');
+} else {
+    alert('No podes ingresar al bar');
+}
