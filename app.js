d2dccb21ae74598c8277f62e00bf26e3be8c2043
Micaela Mazza <micamazza@hotmail.com>
2021-07-31 23:37:40 -0300

    desafio js2

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-let name = prompt(' Ingrese su nombre');
-let age = Number(prompt('Ingrese su edad'))
 
-// condicional
+let name1 = prompt(" Cual es tu nombre?")
+let nota1 = Number(prompt("Cual es tu nota del primer trimestre?"))
+let nota2 =  Number(prompt("Cual es tu nota del segundo trimestre?"))
+ let nota3 =  Number(prompt("Cual es tu nota del tercer trimestre?"))
 
-if (age >= 18) {
-    alert('Podes ingresar al bar');
-} else {
-    alert('No podes ingresar al bar');
-}
+
+ function notaF(nota1, nota2, nota3) {
+     let resultado = nota1 + nota2 + nota3 
+     return resultado
+ }
+
+console.log( name1 + ' tu nota final es de', notaF(nota1, nota2, nota3)/3)
